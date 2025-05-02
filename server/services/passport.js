150dd416047610840f09619a3a16dd609844c865
StarkovaKseniia <dammit.kseniya@gmail.com>
2025-05-02 21:38:54 +0300

    authorization + deployment

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,15 +5,32 @@ const keys = require('../config/keys');
 
 const Player = mongoose.model('players');
 
+passport.serializeUser((player, done) => {
+    done(null, player.id);
+});
+
+passport.deserializeUser((id, done) => {
+    Player.findById(id).then(player => {
+        done(null, player);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
         }, 
         (accessToken, refreshToken, profile, done) => {
-            new Player({ googleId: profile.id }).save(); 
+            Player.findOne({ googleId: profile.id }).then((existingPlayer) => {
+                    if (existingPlayer){
+                        done(null, existingPlayer);
+                    } else {
+                        new Player({ googleId: profile.id }).save().then(player => done(null, player));
+                    }
+                }) 
         }
     )
 );
